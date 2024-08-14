1880dbae372a203719f7af910199ec444fbfb459
corporateready <102033511+corporateready@users.noreply.github.com>
2024-08-14 18:34:21 +0000

    added startServer function

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function startAtSpecificTime(targetHour, targetMinute, targetSecond) {
   }, delay);
 }
 
-startAtSpecificTime(21, 32, 0);
+startAtSpecificTime(21, 36, 0);
 
 // async function getAllHistory() {
 //   try {
