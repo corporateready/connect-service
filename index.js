66452168cf737b198ee92a020c0bf92a058ea4e4
corporateready <102033511+corporateready@users.noreply.github.com>
2024-10-22 07:39:40 +0000

    update index file

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
 import express from "express";
 import "dotenv/config.js";
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import EventEmitter from "events"
 import updateAccessToken from "./service/supabase/data-features/updateAccessToken.js";
 import startAccessToken from "./service/supabase/data-features/startAccessToken.js";
-import handleRudderStackEvent from "./service/rudderstack/handleRudderStackEvent.js";
 import postNewContact from "./service/postNewContact.js";
-// import tokenId from "./service/database/get-data-functions/getTokenId.js";
 import lengthCollectionEvent from "./service/pbx/length-collection-event/lengthCollectionEvent.js";
 
 const eventEmitter = new EventEmitter()
 const app = express();
 const port = 3003;
 
-const supabaseUrl = process.env.NODEJS_SUPABASE_URL;
-const supabaseKey = process.env.NODEJS_SUPABASE_API_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const refreshToken = process.env.NODEJS_REFRESH_TOKEN;
+// const refreshToken = process.env.NODEJS_REFRESH_TOKEN;
 
 function myFunction() {
-  console.log("Function executed!");
-  startAccessToken(refreshToken);
-  setInterval(() => updateAccessToken(refreshToken), 3500 * 1000);
+  console.log("Application was started...!")
+  // startAccessToken(refreshToken);
+ 
 }
 
 eventEmitter.once('myEvent', myFunction);
 
 eventEmitter.emit('myEvent');
-eventEmitter.emit('myEvent');
-// generateTokens(
-//   "1000.ec50a6d17c8f5989384ef504f7877d3c.7731d73285821a47aaea3cb7087e7038"
-// );
+
+ setInterval(() => console.log("Refreshed...!"), 10 * 1000);
 
 app.post("/", async (req, res) => {
   try {
